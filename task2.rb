215734ede71a6a07ad9a6315f2a51223ee86098f
cekynda <cekynd4@gmail.com>
2021-08-22 00:07:04 +0300

    fix task2 and task3

--- task2.rb
+++ task2.rb
@@ -1,19 +1,3 @@
-class Symbol
-  def to_i
-    if self == :first
-      1
-    elsif self == :second
-      2
-    elsif self == :third
-      3
-    end
-  end
-
-end
 arr = %i[first second third]
-
-hash = arr.each_with_object(Hash.new(0)) do |key, value|
-  value[key] += key.to_i
-end
-
-puts hash
+arr = arr.map.with_index { |v,i| [ v, i + 1] }
+p arr.to_h
